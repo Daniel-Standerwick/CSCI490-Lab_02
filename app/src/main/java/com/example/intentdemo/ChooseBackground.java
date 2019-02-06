44d300bbf8453a3d5bf5a42d9bc8a84ef16c6ba1
Daniel Standerwick <standerwickd@gmail.com>
2019-02-06 18:46:06 -0500

    Moving Activitys yet to get the background in main

---
+++ app/src/main/java/com/example/intentdemo/ChooseBackground.java
@@ -0,0 +1,62 @@
+package com.example.intentdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import static com.example.intentdemo.R.drawable.supermoon;
+
+public class ChooseBackground extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView superMoon;
+    ImageView jessus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_background);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        superMoon = findViewById(R.id.superMoon);
+        jessus = findViewById(R.id.JESSUS);
+
+        superMoon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        jessus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
--- app/src/main/java/com/example/intentdemo/MainActivity.java
+++ app/src/main/java/com/example/intentdemo/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.intentdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button goToSelectBackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToSelectBackground = findViewById(R.id.setBackground);
+
+        goToSelectBackground.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ChooseBackground.class);
+                startActivity(i);
+            }
+        });
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
